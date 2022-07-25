357701110ffeed65a79e0dd1b38b3471ce2b8557
DJDERNANE <108133683+DJDERNANE@users.noreply.github.com>
2022-07-26 00:06:13 +0200

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-var navigation = document.getElementById("navigation"),
-header=document.getElementById("header");
-function showList(){
-    navigation.classList.toggle("clicked");
-    header.classList.toggle("clicked")
-}
