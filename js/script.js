9c52a4075193e0eed4d6722ac7ae19130fee38cd
DJDERNANE <108133683+DJDERNANE@users.noreply.github.com>
2022-07-26 00:35:56 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var navigation = document.getElementById("navigation"),
+header=document.getElementById("header");
+function showList(){
+    navigation.classList.toggle("clicked");
+    header.classList.toggle("clicked")
+}
