9e5367629c3793a5985e64deacf9e0cc063e42e7
DJDERNANE <108133683+DJDERNANE@users.noreply.github.com>
2022-12-02 17:27:35 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-var navigation = document.getElementById("navigation"),
-header=document.getElementById("header");
-function showList(){
-    navigation.classList.toggle("clicked");
-    header.classList.toggle("clicked")
+var btn = document.getElementById("btnlist")
+var nav = document.getElementById("navbar")
+var headtop = document.getElementById("headtop") 
+
+btn.addEventListener("click", function(){
+    btn.classList.toggle("active")
+    nav.classList.toggle("active")
+    headtop.classList.toggle("active")
+})
+window.onscroll = function(){
+    if(window.scrollY >= (screen.height - 500)){
+        headtop.classList.add("scrolled")
+    }
+    else{
+        headtop.classList.remove("scrolled")
+    }
+   
 }
